5c7578645f32a1e4c2b934042f20cae98b2b677d
cxm <18255117203@163.com>
2019-05-06 11:25:47 +0800

    vue demo2

---
+++ src/views/Add.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <p>标题</p>
+    <input type="text" v-model="title">
+    <p>内容</p>
+    <input type="text" v-model="content">
+    <div class="button" @click="add()">添加</div>
+  </div>
+</template>
+
+<script>
+    import store from '@/store'
+    export default {
+        name: 'add',
+        data () {
+            return {
+                title: '',
+                content: ''
+            }
+        },
+        methods: {
+            add() {
+                store.commit('addItems',{
+                    titile: this.title,
+                    content: this.content
+                })
+                this.title = '';
+                this.content = '';
+                this.$router.push('/home/list');
+            }
+        }
+    }
+</script>
+
